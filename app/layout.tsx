e758dcee58814ad4c9e29af35546285315b21bc2
ForrestCogna <forrest@cogna.co>
2025-05-20 00:22:44 +0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DevControlsWrapper from "./components/DevControlsWrapper";
 import { PostHogProvider } from "./components/PostHogProvider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <PostHogProvider>
           {children}
           <DevControlsWrapper />
